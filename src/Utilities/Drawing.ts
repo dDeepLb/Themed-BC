142387da608e9662f37c0f1e8987a5d95a84cfdd
dDeepLb <ddeeplb@gmail.com>
2024-05-26 18:25:39 +0300

    change Image class into object

--- src/Utilities/Drawing.ts
+++ src/Utilities/Drawing.ts
@@ -1,16 +1,16 @@
 let cachedImages = {};
 
-export class _Image {
-  static doNotDrawImageFolders = [
+export const _Image = {
+  doNotDrawImageFolders: [
     "Assets/Female3DCG/",
     "Backgrounds/",
     "Icons/Struggle/",
     "Screens/",
     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyEAYAAABOr1TyAAABb2lDQ1BpY2MAACiRdZG9S0JRGMZ/", // FBC's "FBC" overlay icon
     "http" // Fix bug with recoloring of custom bgs
-  ];
+  ],
 
-  static doNotDrawImages = [
+  doNotDrawImages: [
     "Icons/Accept.png",
     "Icons/Activity.png",
     "Icons/Arousal.png",
@@ -50,29 +50,23 @@ export class _Image {
     "Icons/Small/YouTube.png",
     "Assets/Female3DCG/ItemMisc/Preview/Best Friend Padlock.png",
     "Assets/Female3DCG/ItemMisc/Preview/Best Friend Timer Padlock.png"
-  ];
+  ],
 
-  static drawColorized(source: string, x: number, y: number, hexColor: string, { newWidth = null, newHeight = null }) {
+  getColorized(source: string, hexColor: string) {
     if (typeof source != "string") return;
     const img = DrawGetImage(source);
 
-    // Check if the colorized image is already cached
     if (!!_Image.getCache(source)) {
-      _Image.drawCached(source, x, y, img, { newWidth, newHeight });
-      return true;
+      return _Image.getCache(source);
     }
 
     try {
-      // If not cached, load the original image
-      // Make sure that the starting image is loaded
       if (!img.complete) return false;
-      if (!img.naturalWidth) return true;
+      if (!img.naturalWidth) return false;
 
-      // Continue with the colorization process
       const width = img.width;
       const height = img.height;
 
-      // Prepare a canvas to draw the colorized image
       ColorCanvas.canvas.width = width;
       ColorCanvas.canvas.height = height;
       ColorCanvas.globalCompositeOperation = "copy";
@@ -80,89 +74,71 @@ export class _Image {
 
       const imageData = ColorCanvas.getImageData(0, 0, width, height);
 
-      // Colorize the plain image
-      _Image.colorize(imageData, hexColor);
+      const colorizedData = _Image.colorize(imageData, hexColor);
 
-      // Cache the colorized image
-      _Image.setCache(source, imageData);
+      _Image.setCache(source, colorizedData);
 
-      // Draw the colorized image
-      _Image.drawCached(source, x, y, img, { newWidth, newHeight });
-
-      return true;
+      return colorizedData;
     } catch (e) {
       return false;
     }
-  }
+  },
 
-  static colorize(imageData: ImageData, color: string) {
+  turnToBase64(imageData: ImageData) {
+    const canvas = document.createElement("canvas");
+    canvas.width = imageData.width;
+    canvas.height = imageData.height;
+    const ctx = canvas.getContext("2d");
+    ctx.putImageData(imageData, 0, 0);
+    return canvas.toDataURL("image/png");
+  },
+
+  colorize(imageData: ImageData, hexColor: string) {
     const data = imageData.data;
 
-    // Get the RGB color used to transform
-    const rgbColor = DrawHexToRGB(color);
+    const rgbColor = DrawHexToRGB(hexColor);
 
-    // We transform each non-transparent pixel based on the RGB value
-    for (let p = 0, len = data.length; p < len; p += 4) {
-      if (data[p + 3] == 0) continue;
-      const trans = (data[p] + data[p + 1] + data[p + 2]) / 383;
-      data[p + 0] = rgbColor.r * trans;
-      data[p + 1] = rgbColor.g * trans;
-      data[p + 2] = rgbColor.b * trans;
+    for (let pixelData = 0, len = data.length; pixelData < len; pixelData += 4) {
+      if (data[pixelData + 3] == 0) continue;
+      const transparency = (data[pixelData] + data[pixelData + 1] + data[pixelData + 2]) / 383;
+      data[pixelData + 0] = rgbColor.r * transparency;
+      data[pixelData + 1] = rgbColor.g * transparency;
+      data[pixelData + 2] = rgbColor.b * transparency;
     }
-  }
 
-  static drawCached(source: string, x: number, y: number, img: HTMLImageElement, { newWidth = null, newHeight = null }) {
-    const colorizedImage = cachedImages[source];
-    const sizeChanged = newWidth != null || newHeight != null;
+    return imageData;
+  },
 
-    const width = img.width;
-    const height = img.height;
-
-    ColorCanvas.canvas.width = width;
-    ColorCanvas.canvas.height = height;
-
-    ColorCanvas.putImageData(colorizedImage, 0, 0);
-
-    MainCanvas.save();
-    MainCanvas.globalAlpha = 1;
-    MainCanvas.globalCompositeOperation = "source-over";
-    if (!sizeChanged) MainCanvas.drawImage(ColorCanvas.canvas, 0, 0, img.width, img.height, x, y, img.width, img.height);
-    else MainCanvas.drawImage(ColorCanvas.canvas, 0, 0, img.width, img.height, x, y, newWidth, newHeight);
-    MainCanvas.restore();
-  }
-
-  static doDrawImage(source: string) {
+  doDrawImage(source: string) {
     let skipDrawing = false;
 
-    // Check if the image is in any excluded folder
     for (const folderPrefix of _Image.doNotDrawImageFolders) {
       if (typeof source !== "string") break;
       if (source.startsWith(folderPrefix)) {
-        skipDrawing = true; // Do not draw if it starts with any excluded path
+        skipDrawing = true;
         break;
       }
     }
 
-    // Check if the image is in the list of excluded images
     if (!skipDrawing && _Image.doNotDrawImages.includes(source)) {
       skipDrawing = true;
     }
 
     return !skipDrawing;
-  }
+  },
 
-  static setCache(key: string, data: ImageData) {
+  setCache(key: string, data: ImageData): void {
     cachedImages[key] = data;
-  }
+  },
 
-  static getCache(key: string) {
+  getCache(key: string): ImageData {
     return cachedImages[key];
-  }
+  },
 
-  static clearCache() {
+  clearCache(): void {
     cachedImages = {};
-  }
-}
+  },
+};
 
 export function drawRect(x: number, y: number, width: number, height: number, backgroundColor: string, borderColor: string) {
   DrawRect(x, y, width, height, backgroundColor);
