c833069e70085bb15668dc4ad43f5a69357ee845
dDeepLb <ddeeplb@gmail.com>
2024-08-12 07:00:25 +0300

    some building improvement

---
+++ build.mjs
@@ -0,0 +1,50 @@
+import { build, context } from 'esbuild';
+import copy from 'esbuild-copy-files-plugin';
+import progress from 'esbuild-plugin-progress';
+import time from 'esbuild-plugin-time';
+
+(async () => {
+  const PORT = 1001;
+  const HOST = 'localhost';
+  const devPath = `http://${HOST}:${PORT}`;
+  const prodPath = 'https://ddeeplb.github.io/Themed-BC';
+  const isDev = process.argv.includes('--dev');
+  const PUBLIC_URL = isDev ? devPath : prodPath;
+
+  const ctx = await (isDev ? context : build)({
+    entryPoints: ['./src/Themed.ts'],
+    outfile: './dist/themed.js',
+    format: 'iife',
+    globalName: 'Themed',
+    bundle: true,
+    sourcemap: true,
+    target: ['es2020'],
+    loader: {
+      '.html': 'text',
+      '.css': 'text',
+      '.png': 'dataurl'
+    },
+    treeShaking: true,
+    define: { serverUrl: JSON.stringify(PUBLIC_URL) },
+    plugins: [
+      copy({
+        source: ['./src/Translations'],
+        target: './dist/',
+        copyWithFolder: true
+      }),
+      progress(),
+      time(),
+    ],
+  }).catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+  if (isDev) {
+    await ctx.watch();
+    console.info('Watching for changes...');
+
+    const server = await ctx.serve({ host: HOST, port: PORT, servedir: 'dist' });
+    console.info(`Server running at ${server.host}:${server.port}`);
+  }
+})();
