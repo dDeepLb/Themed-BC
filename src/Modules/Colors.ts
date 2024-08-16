b4407905976dba42278fce30a14fcb03d03b69ac
dDeepLb <ddeeplb@gmail.com>
2024-08-16 20:15:49 +0300

    set crude defaults for colors

--- src/Modules/Colors.ts
+++ src/Modules/Colors.ts
@@ -1,3 +1,4 @@
+import Color from 'color';
 import { BaseModule } from '../Base/BaseModule';
 import { getModule } from '../Base/Modules';
 import { Subscreen } from '../Base/SettingDefinitions';
@@ -9,6 +10,20 @@ import { changeModColors } from '../Utilities/Integration';
 import { deepMergeMatchingProperties } from '../Utilities/Other';
 import { BcStyle } from '../Utilities/Style';
 import { GuiRedrawModule } from './GuiRedraw';
+
+const primaryColor = Color('#202020');
+const elementColor = primaryColor.lighten(0.2);
+const accentColor = Color('#440171');
+const textColor = Color('#cccccc');
+const specialColors = {
+  equipped: Color('#ADD8E6'),
+  disabled: Color('#808080'),
+  crafted: Color('#FFFFAF'),
+  blocked: Color('#b81010'),
+  limited: Color('#ffA500'),
+  allowed: Color('#00FF00'),
+  friendRoom: Color('#00FF00'),
+};
 
 export class ColorsModule extends BaseModule {
   get settingsScreen(): Subscreen | null {
@@ -25,9 +40,31 @@ export class ColorsModule extends BaseModule {
 
   get defaultSettings() {
     return <ColorsSettingsModel>{
-      primaryColor: '#202020',
-      accentColor: '#440171',
-      textColor: '#ccc'
+      base: {
+        main: primaryColor.hex(),
+        accent: accentColor.hex(),
+        text: textColor.hex(),
+      },
+      advanced: {
+        main: primaryColor.hex(),
+        element: elementColor.hex(),
+        elementHover: elementColor.lighten(0.2).hex(),
+        elementDisabled: elementColor.darken(0.2).hex(),
+        elementHint: elementColor.lighten(0.2).hex(),
+        accent: accentColor.hex(),
+        accentHover: accentColor.lighten(0.2).hex(),
+        accentDisabled: accentColor.darken(0.2).hex(),
+        text: textColor.hex(),
+      },
+      special: {
+        equipped: specialColors.equipped.hex(),
+        disabled: specialColors.disabled.hex(),
+        crafted: specialColors.crafted.hex(),
+        blocked: specialColors.blocked.hex(),
+        limited: specialColors.limited.hex(),
+        allowed: specialColors.allowed.hex(),
+        friendRoom: specialColors.friendRoom.hex(),
+      },
     };
   }
 
