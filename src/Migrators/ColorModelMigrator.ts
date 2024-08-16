0a2b7e224a6b52da864fe55b2478ddcc37c738c7
dDeepLb <ddeeplb@gmail.com>
2024-08-16 21:54:09 +0300

    change version due to big changes. and rename props

--- src/Migrators/ColorModelMigrator.ts
+++ src/Migrators/ColorModelMigrator.ts
@@ -2,19 +2,19 @@ import { BaseMigrator } from './BaseMigrator';
 
 export class ColorModelMigrator extends BaseMigrator {
   get MigrationVersion(): string {
-    return '1.3.5';
+    return '1.4.0';
   }
 
   Migrate(): boolean {
     if (!Player.Themed.ColorsModule) return false;
 
     if (Player.Themed.ColorsModule['primaryColor']) {
-      Player.Themed.ColorsModule.base.mainBackground = Player.Themed.ColorsModule['primaryColor'];
+      Player.Themed.ColorsModule.base.main = Player.Themed.ColorsModule['primaryColor'];
       delete Player.Themed.ColorsModule['primaryColor'];
     }
 
     if (Player.Themed.ColorsModule['accentColor']) {
-      Player.Themed.ColorsModule.base.icon = Player.Themed.ColorsModule['accentColor'];
+      Player.Themed.ColorsModule.base.accent = Player.Themed.ColorsModule['accentColor'];
       delete Player.Themed.ColorsModule['accentColor'];
     }
 
