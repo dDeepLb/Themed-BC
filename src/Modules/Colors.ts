22163fba4166587cc637df711bdb2cb2c256febb
dDeepLb <ddeeplb@gmail.com>
2024-08-28 21:27:28 +0300

    clear up color.ts by removing functions replaced by color library. remove caching

--- src/Modules/Colors.ts
+++ src/Modules/Colors.ts
@@ -5,7 +5,6 @@ import { Subscreen } from '../Base/SettingDefinitions';
 import { ColorsSettingsModel } from '../Models/Colors';
 import { GuiColors } from '../Screens/Colors';
 import { _Color } from '../Utilities/Color';
-import { _Image } from '../Utilities/Drawing';
 import { changeModColors } from '../Utilities/Integration';
 import { deepMergeMatchingProperties } from '../Utilities/Other';
 import { BcStyle } from '../Utilities/Style';
@@ -78,7 +77,6 @@ export class ColorsModule extends BaseModule {
     _Color.composeRoot();
     BcStyle.reloadAll();
     changeModColors();
-    _Image.clearCache();
     getModule<GuiRedrawModule>('GuiRedrawModule').toggleGuiPatches();
   }
 }
