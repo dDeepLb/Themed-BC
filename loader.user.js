f9cac2b5a590d289c06091f6fcabe4470a5eefcb
dDeepLb <71733861+dDeepLb@users.noreply.github.com>
2024-09-02 10:34:47 +0300

    Add development version (#15)
    * add dev version
    
    * update lock file

--- loader.user.js
+++ loader.user.js
@@ -8,8 +8,10 @@
 // @match https://www.bondageprojects.elementfx.com/*
 // @match https://bondage-europe.com/*
 // @match https://www.bondage-europe.com/*
-// @match http://localhost:*/*
-// @match http://127.0.0.1:*/*
+// @match https://bondageprojects.com/*
+// @match https://www.bondageprojects.com/*
+// @match http://localhost:55555/*
+// @match http://localhost/BondageClub/*
 
 // @grant none
 // @run-at document-end
@@ -17,9 +19,22 @@
 
 (function () {
   "use strict";
+  const ending = 'themed.js';
+  const prodPath = 'https://ddeeplb.github.io/Themed-BC/';
+  const devPath = `${prodPath}dev/`;
+  const localPath = 'http://localhost:1001/';
+
+  const isDev = window.location.search.includes('TMD=dev');
+  const isLocal = window.location.search.includes('TMD=local');
+  const isPublic = isDev || !isLocal;
+
+  let modLink = prodPath;
+  if (isDev) modLink = devPath;
+  else if (isLocal) modLink = localPath;
+
   const script = document.createElement("script");
   script.type = "module";
   script.setAttribute("crossorigin", "anonymous");
-  script.src = `https://ddeeplb.github.io/Themed-BC/themed.js?v=${Date.now()}`;
+  script.src = `${modLink}${ending}${isPublic ? '' : '?' + Date.now()}`;
   document.head.appendChild(script);
 })();
