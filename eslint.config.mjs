38cbd04ba16fe6ec1f3afaf5ff3f0a63f981b3a2
dDeepLb <ddeeplb@gmail.com>
2024-09-17 18:14:24 +0300

    fix broken screen loading

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ export default tseslint.config(
       'style/indent': ['warn', 2],
       'style/quotes': ['warn', 'single'],
       'style/semi': ['error', 'always'],
-      'style/linebreak-style': ['warn', 'unix'],
+      'style/linebreak-style': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': ['warn'],
     },
