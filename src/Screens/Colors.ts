7573edef51ce69bd2cb85eb1e2884f3b7503fe7d
dDeepLb <ddeeplb@gmail.com>
2025-04-29 18:17:16 +0300

    implement live reload for theme colors

--- src/Screens/Colors.ts
+++ src/Screens/Colors.ts
@@ -4,8 +4,11 @@ import { getModule } from '../Base/Modules';
 import { BaseColorsModel, ColorsSettingsModel } from '../Models/Colors';
 import { ColorsModule } from '../Modules/Colors';
 import { getText } from '../Translation';
+import { _Color } from '../Utilities/Color';
 
 export class GuiColors extends GuiSubscreen {
+  settingsBackup: ColorsSettingsModel;
+
   get name(): string {
     return 'Colors';
   }
@@ -29,7 +32,6 @@ export class GuiColors extends GuiSubscreen {
       label: `colors.setting.${key}.name`,
       description: `colors.setting.${key}.desc`,
       setting: () => value ?? defaultSettings.base[key],
-      setSetting: (val) => (this.settings.base[key] = val),
       disabled: isBaseMode && !baseModeKey(key)
     })).sort((a, b) => (a.disabled ? 1 : 0) - (b.disabled ? 1 : 0)) as Input[],
     Object.entries(this.settings.special).map(([key, value]) => ({
@@ -38,8 +40,41 @@ export class GuiColors extends GuiSubscreen {
       label: `colors.setting.${key}.name`,
       description: `colors.setting.${key}.desc`,
       setting: () => value ?? defaultSettings.special[key],
-      setSetting: (val) => (this.settings.special[key] = val)
     }))];
+  }
+
+  Load(): void {
+    super.Load();
+
+    this.settingsBackup = CommonCloneDeep(this.settings);
+
+    const settings = getModule<ColorsModule>('ColorsModule').settings;
+
+    Object.entries(this.settings.base).forEach(([key]) => {
+      (document.getElementById(key) as HTMLInputElement).addEventListener('input', function(ev) {
+        if(!_Color.isValidHex(this.value)) {
+          this.setCustomValidity('Invalid hex color');
+        } else {
+          this.setCustomValidity('');
+          settings.base[key] = this.value;
+        }
+        
+        getModule<ColorsModule>('ColorsModule').reloadTheme();
+      });
+    }),
+
+    Object.entries(this.settings.special).forEach(([key]) => {
+      (document.getElementById(key) as HTMLInputElement).addEventListener('input', function(ev) {
+        if(!_Color.isValidHex(this.value)) {
+          this.setCustomValidity('Invalid hex color');
+        } else {
+          this.setCustomValidity('');
+          settings.special[key] = this.value;
+        }
+        
+        getModule<ColorsModule>('ColorsModule').reloadTheme();
+      });
+    });
   }
 
   Run(): void {
@@ -67,5 +102,27 @@ export class GuiColors extends GuiSubscreen {
     }
 
     super.Click();
+  }
+
+  Exit(): void {
+    const settings = getModule<ColorsModule>('ColorsModule').settings;
+
+    Object.entries(this.settings.base).forEach(([key]) => {
+      const input = document.getElementById(key) as HTMLInputElement;
+
+      if(!_Color.isValidHex(input.value)) {
+        settings.base[key] = this.settingsBackup.base[key];
+      }
+    }),
+
+    Object.entries(this.settings.special).forEach(([key]) => {
+      const input = document.getElementById(key) as HTMLInputElement;
+
+      if(!_Color.isValidHex(input.value)) {
+        settings.special[key] = this.settingsBackup.special[key];
+      }
+    });
+
+    super.Exit();
   }
 }
