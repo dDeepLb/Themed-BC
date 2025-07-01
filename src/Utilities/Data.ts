d0de697de5aa3c3ec6cbfc6274a91f610965857e
dDeepLb <ddeeplb@gmail.com>
2025-07-01 18:36:31 +0300

    remove things that were replaced by library

--- src/Utilities/Data.ts
+++ src/Utilities/Data.ts
@@ -1,36 +1,6 @@
 import { LocalSettingsModel } from '../Models/local';
 import { SettingsModel } from '../Models/Settings';
 import { ModName } from './ModDefinition';
-import { _String } from './String';
-
-export const PlayerStorage = () => (typeof Player?.[ModName] === 'object' ? CommonCloneDeep(Player?.[ModName]) : undefined);
-export const ExtensionStorage = () => Player.ExtensionSettings[ModName];
-
-export function settingsLoad() {
-  if (ExtensionStorage()) {
-    Player[ModName] = JSON.parse(LZString.decompressFromBase64(ExtensionStorage())) as SettingsModel;
-  } else if (Player.OnlineSettings[ModName]) {
-    Player[ModName] = JSON.parse(LZString.decompressFromBase64(Player.OnlineSettings[ModName]));
-
-    delete Player.OnlineSettings[ModName];
-    window.ServerAccountUpdate.QueueData({ OnlineSettings: Player.OnlineSettings });
-  } else {
-    Player[ModName] = <SettingsModel>{};
-  }
-}
-
-export function settingsSave() {
-  if (!ExtensionStorage()) Player.ExtensionSettings[ModName] = '';
-  const Data: SettingsModel = {
-    Version: PlayerStorage().Version,
-    GlobalModule: PlayerStorage().GlobalModule,
-    ColorsModule: PlayerStorage().ColorsModule,
-    IntegrationModule: PlayerStorage().IntegrationModule,
-    ProfilesModule: PlayerStorage().ProfilesModule
-  };
-  Player.ExtensionSettings[ModName] = _String.encode(Data);
-  ServerPlayerExtensionSettingsSync(ModName);
-}
 
 export function settingsReset() {
   Player[ModName] = <SettingsModel>{};
--- src/Utilities/Style.ts
+++ src/Utilities/Style.ts
@@ -19,47 +19,6 @@ const styles: styles = {
   TTS: '',
 };
 
-export const Style = {
-  injectInline(styleId: string, styleSource: string) {
-    const isStyleLoaded = document.getElementById(styleId);
-
-    if (isStyleLoaded) return;
-
-    const styleElement = document.createElement('style');
-    styleElement.id = styleId;
-    styleElement.appendChild(document.createTextNode(styleSource));
-    document.head.appendChild(styleElement);
-  },
-
-  injectEmbed(styleId: string, styleLink: string) {
-    const isStyleLoaded = document.getElementById(styleId);
-
-    if (isStyleLoaded) return;
-
-    const styleElement = document.createElement('link');
-    styleElement.id = styleId;
-    styleElement.rel = 'stylesheet';
-    styleElement.href = styleLink;
-    document.head.appendChild(styleElement);
-  },
-
-  eject(id: string) {
-    const style = document.getElementById(id);
-    if (!style) return;
-
-    style.remove();
-  },
-
-  reload(styleId: string, styleSource: string) {
-    Style.eject(styleId);
-    Style.injectInline(styleId, styleSource);
-  },
-
-  async fetch(link: string) {
-    return fetch(link).then((res) => res.text());
-  }
-};
-
 export const BcStyle = {
   injectAll() {
     const isEnabled = PlayerStorage().GlobalModule.themedEnabled;
