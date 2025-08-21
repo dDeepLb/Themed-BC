1a0df1a5fc65e731429b06835d4f6a288ccc4c6a
dDeepLb <ddeeplb@gmail.com>
2025-08-21 18:07:48 +0300

    move setting up default values to dedicated function to prevent some crashes

--- src/Modules/Profiles.ts
+++ src/Modules/Profiles.ts
@@ -1,7 +1,6 @@
 import { BaseModule, deepMergeMatchingProperties, getModule, modStorage, Subscreen } from 'bc-deeplib/deeplib';
 import { ProfileSaveModel, ProfilesSettingsModel } from '../Models/Profiles';
 import { GuiProfiles } from '../Screens/Profiles';
-import { ModName } from '../Utilities/ModDefinition';
 import { ColorsModule } from './Colors';
 import { GlobalModule } from './Global';
 import { IntegrationModule } from './Integration';
@@ -20,26 +19,30 @@ export class ProfilesModule extends BaseModule {
   }
 
   get defaultSettings() {
-    return <ProfilesSettingsModel>{};
-  }
-
-  load(): void {
     const profileDefaults: ProfileSaveModel = {
       GlobalModule: getModule<GlobalModule>('GlobalModule').defaultSettings,
       ColorsModule: getModule<ColorsModule>('ColorsModule').defaultSettings,
       IntegrationModule: getModule<IntegrationModule>('IntegrationModule').defaultSettings
     };
+
+    const data = modStorage.playerStorage?.ProfilesModule || {};
     
     for (let i = 0; i < 3; i++) {
       const profileIndex = i + 1;
-      if (!modStorage.playerStorage.ProfilesModule[profileIndex] || Object.keys(modStorage.playerStorage.ProfilesModule[profileIndex]).length === 0) {
-        Player[ModName].ProfilesModule[profileIndex] = {
+      if (!data[profileIndex] || Object.keys(data[profileIndex]).length === 0) {
+        data[profileIndex] = {
           data: <ProfileSaveModel>{},
           name: ''
         };
       }
       
-      if (Object.keys(Player[ModName].ProfilesModule[profileIndex].data).length > 0) Player[ModName].ProfilesModule[profileIndex].data = deepMergeMatchingProperties<ProfileSaveModel>(profileDefaults, Player[ModName].ProfilesModule[profileIndex].data);
+      if (Object.keys(data[profileIndex].data).length > 0) 
+        data[profileIndex].data = deepMergeMatchingProperties<ProfileSaveModel>(profileDefaults, data[profileIndex].data);
     }
+
+    return data;
+  }
+
+  load(): void {
   }
 }
