30668ee218a42405045ca366dbcab99384e10237
dDeepLb <ddeeplb@gmail.com>
2025-10-19 17:16:50 +0300

    bump version, update changelog

--- loader.user.js
+++ loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Themed BC
 // @namespace https://www.bondageprojects.com/
-// @version 1.6.1
+// @version 1.6.2
 // @description Bondage Club with themes!
 // @author  dDeepLb
 // @match https://bondageprojects.elementfx.com/*
