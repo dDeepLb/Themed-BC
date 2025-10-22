28d6b3f8650e2f7c130668efc8fcc03964fbb054
dDeepLb <71733861+dDeepLb@users.noreply.github.com>
2025-10-22 18:28:46 +0000

    Deploying to gh-pages from @ dDeepLb/Themed-BC@c6ef713722a7303691ea835692804690a4a8b328 🚀

--- themed.js
+++ themed.js
@@ -5104,7 +5104,7 @@ One of mods you are using is using an old version of SDK. It will work for now b
   var GuiColors = _GuiColors;
 
   // src/utilities/mod_definition.ts
-  var MOD_VERSION_CAPTION = false ? `${"1.6.2"} - ${"30668ee2"}` : "1.6.2";
+  var MOD_VERSION_CAPTION = false ? `${"1.6.2"} - ${"c6ef7137"}` : "1.6.2";
   var ModuleCategory = {
     Global: "Global",
     Colors: "Colors",
